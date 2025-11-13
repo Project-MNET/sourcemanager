1a9f55239f89eae6b96ce21369e86cdc5deb356e
Mauno Pasanen <mepasane@gmail.com>
2025-11-13 17:00:58 +0200

    added .pylint

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
+import os
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
 from dotenv import load_dotenv
-import os
 from forms import ReferenceForm
 
 
